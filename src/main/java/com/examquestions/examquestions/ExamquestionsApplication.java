a44fa99d7a5afb7c3a2b42ab71c3ea7b107f71d7
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-05-05 11:02:02 +0300

    Initial commit

---
+++ src/main/java/com/examquestions/examquestions/ExamquestionsApplication.java
@@ -0,0 +1,13 @@
+package com.examquestions.examquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamquestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamquestionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/examquestions/examquestions/Question.java
@@ -0,0 +1,7 @@
+package com.examquestions.examquestions;
+
+import java.util.Objects;
+
+public class Question {
+
+}
